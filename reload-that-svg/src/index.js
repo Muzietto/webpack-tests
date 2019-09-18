b5556df099069d1f090653b8cd412f27ff8d512a
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2019-09-18 13:03:08 +0200

    completed the sample svg page

--- reload-that-svg/src/index.js
+++ reload-that-svg/src/index.js
@@ -5,3 +5,12 @@ console.log('got it');
 $('#main').css({
   border: '3px dashed red',
 });
+
+setTimeout(() => {
+  $('#london text').text('No longer London');
+},1000);
+
+// if we want to manipulate svg elements we gotta load https://github.com/kbwood/svg
+setTimeout(() => {
+  $('#paris').css({ border: '3px solid red' });
+},2000);
