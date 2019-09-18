5c5d86c7bf027731af1e454960b9b8ac322d669e
Luigi Colombi <luigi.colombi@easywelfare.com>
2019-09-18 16:45:59 +0200

    first try to change svg fill color when room is busy

--- reload-that-svg/src/index.js
+++ reload-that-svg/src/index.js
@@ -1,6 +1,6 @@
+const moment = require('moment');
 
-console.log('got it');
-console.log('got it');
+moment.locale('it');
 
 const GOOGLE_CLIENT_ID = '953923587361-l0hislojka0lp6osmgdllr0s0g7gs1vf.apps.googleusercontent.com';
 const GOOGLE_API_KEY = 'AIzaSyCAHomCUKuMS48g2g8czmbOmQGpdZIubic';
@@ -13,22 +13,14 @@ $('#main').css({
   border: '3px dashed red',
 });
 
-setTimeout(() => {
-  $('#london text').text('No longer London');
-}, 1000);
+// setTimeout(() => {
+//   $('#london text').text('No longer London');
+// }, 1000);
 
 // if we want to manipulate svg elements we gotta load https://github.com/kbwood/svg
 setTimeout(() => {
   $('#paris').css({ border: '3px solid red' });
 }, 2000);
-
-
-
-$(function () {
-
-  console.log(document.getElementById('paris'));
-
-});
 
 function handleLoadGoogleApiScript() {
   gapi.load('client:auth2', initGoogleClient);
@@ -65,7 +57,9 @@ function handleGoogleAuthClick(event) {
 }
 
 function fetchTodayRoomStatus() {
-  const today = new Date();
+  const todayStartDateTime = moment().startOf('day');
+  const todayEndDateTime = moment().endOf('day'); 
+
   gapi.client.calendar.freebusy.query({
     items: [
       { id: 'easywelfare.com_3435383934323433353736@resource.calendar.google.com' }, // sala londra
@@ -75,11 +69,38 @@ function fetchTodayRoomStatus() {
       { id: 'easywelfare.com_343630323636352d363735@resource.calendar.google.com' }, // sala berlino
       { id: 'easywelfare.com_3437363934393835393631@resource.calendar.google.com' }  // sala parigi
     ],
-    timeMin: (new Date(2019, 8, 16)).toISOString(),
-    timeMax: (new Date(2019, 8, 17)).toISOString()
+    timeMin: todayStartDateTime,
+    timeMax: todayEndDateTime,
   }).then(function(response) {
     console.log('freebusy: ', response);
+
+    const now = moment();
+
+    if (response && response.result && response.result.calendars) {
+      const { calendars } = response.result;
+      const parisRoomBusyCalendar = calendars['easywelfare.com_3437363934393835393631@resource.calendar.google.com'];
+
+      const isParisRoomBusy = parisRoomBusyCalendar.busy.filter(busy => (now.isSameOrAfter(moment(busy.start)) && now.isSameOrBefore(moment(busy.end))) );
+
+      if (isParisRoomBusy.length > 0) {
+        $('#paris').css({ fill: "#ff0000" });
+      }
+
+      const londonRoomBusyCalendar = calendars['easywelfare.com_3435383934323433353736@resource.calendar.google.com'];
+
+      const isLondonRoomBusy = londonRoomBusyCalendar.busy.filter(busy => (now.isSameOrAfter(moment(busy.start)) && now.isSameOrBefore(moment(busy.end))) );
+
+      if (isLondonRoomBusy.length > 0) {
+        $('#london').css({ fill: "#ff0000" });
+      }
+    }
+    
   });
 }
+
+$("#paris").on("click",function(){
+  console.log('click');
+  $("#paris").attr("background","#ff0000");   
+});
 
 window.handleLoadGoogleApiScript = handleLoadGoogleApiScript;
