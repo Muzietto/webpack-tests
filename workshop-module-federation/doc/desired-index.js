3d3c85daf4ca7996db9bade4d67f3962f1a2c12e
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2021-11-16 10:53:14 +0100

    05-basic-module-federation: desired index.js in shell project

---
+++ workshop-module-federation/doc/desired-index.js
@@ -0,0 +1,4 @@
+import greetingInRootDiv from 'module-federation-mfe/greetingInRootDiv';
+import $ from 'jquery';
+
+greetingInRootDiv($('#shellRoot'), 'SHELL - please check window.jQuery.fn.greenify in the console');
